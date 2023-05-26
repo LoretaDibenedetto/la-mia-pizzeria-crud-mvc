b6089af0e69ffae50e9d287c6d931dc370281f72
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-05-26 15:49:03 +0200

    add review create action and view

--- LaMiaPizzeria/Controllers/ReviewController.cs
+++ LaMiaPizzeria/Controllers/ReviewController.cs
@@ -8,11 +8,53 @@ namespace LaMiaPizzeria.Controllers
     {
         public IActionResult Index()
         {
-            using(PizzaContext db =  new PizzaContext())
+            using (PizzaContext db = new PizzaContext())
             {
                 List<Review> reviews = db.Reviews.ToList();
                 return View(reviews);
             }
         }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Review review)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("Create" ,review);
+            }
+            using (PizzaContext db = new PizzaContext())
+            {
+                Review reviewToCreate = new Review();
+                reviewToCreate.Name = review.Name;
+                reviewToCreate.Description = review.Description;    
+                db.Reviews.Add(reviewToCreate);
+                db.SaveChanges();   
+                return RedirectToAction("Index");   
+            }
+        
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
